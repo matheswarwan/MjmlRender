d82ec31beeee1c78568ee1a1034ff3d126a9fcd1
Matheswaran K <mathes.btech@gmail.com>
2022-09-09 17:07:23 -0700

    updates

--- js/helper.js
+++ js/helper.js
@@ -287,56 +287,12 @@ function sanitiseHtml(htmlOutput) {
     });
     
     htmlOutput = htmlObject.querySelector('div').innerHTML
-    htmlOutput = formatCode(htmlOutput, true, true);
+    var config = { indent_size: 2, space_in_empty_paren: false, keep_array_indentation: true }
+    htmlOutput = html_beautify(htmlOutput,config);
     console.log('formatted code ')
     console.log(htmlOutput)
     return htmlOutput;
 }
-
-// Original: https://jsfiddle.net/buksy/rxucg1gd/
-// Parameters:
-// code 					- (string) code you wish to format
-// stripWhiteSpaces			- (boolean) do you wish to remove multiple whitespaces coming after each other?
-// stripEmptyLines 			- (boolean) do you wish to remove empty lines?
-var formatCode = function(code, stripWhiteSpaces, stripEmptyLines) {
-    "use strict";
-    var whitespace          = ' '.repeat(2);             // Default indenting 4 whitespaces
-    var currentIndent       = 0;
-    var char                = null;
-    var nextChar            = null;
-
-
-    var result = '';
-    for(var pos=0; pos <= code.length; pos++) {
-        char            = code.substr(pos, 1);
-        nextChar        = code.substr(pos+1, 1);
-
-        // If opening tag, add newline character and indention
-        if(char === '<' && nextChar !== '/') {
-            result += '\n' + whitespace.repeat(currentIndent);
-            currentIndent++;
-        }
-        // if Closing tag, add newline and indention
-        else if(char === '<' && nextChar === '/') {
-            // If there're more closing tags than opening
-            if(--currentIndent < 0) currentIndent = 0;
-            result += '\n' + whitespace.repeat(currentIndent);
-        }
-
-        // remove multiple whitespaces
-        else if(stripWhiteSpaces === true && char === ' ' && nextChar === ' ') char = '';
-        // remove empty lines
-        else if(stripEmptyLines === true && char === '\n' ) {
-            //debugger;
-            if(code.substr(pos, code.substr(pos).indexOf("<")).trim() === '' ) char = '';
-        }
-
-        result += char;
-    }
-
-    return result;
-}
-
 
 function renderMjml () {
 document.getElementById("errorMsg").innerHTML = "";
